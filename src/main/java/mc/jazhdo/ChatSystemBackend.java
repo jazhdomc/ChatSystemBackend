63d1ba9e6b67d80586ce96dff80d1a6cdcebad81
Jazhdo <jaysonzdong@icloud.com>
2026-07-30 16:14:09 -0700

    v0.1.1 Fix huge bugs

--- src/main/java/mc/jazhdo/ChatSystemBackend.java
+++ src/main/java/mc/jazhdo/ChatSystemBackend.java
@@ -28,7 +28,7 @@ public class ChatSystemBackend extends JavaPlugin implements Listener, PluginMes
         // Register chat listener and plugin messenger
         Server server = getServer();
         Messenger messenger = server.getMessenger();
-        messenger.registerIncomingPluginChannel(this, channel, null);
+        messenger.registerIncomingPluginChannel(this, channel, this);
         messenger.registerOutgoingPluginChannel(this, channel);
         server.getPluginManager().registerEvents(this, this);
     }
@@ -45,6 +45,7 @@ public class ChatSystemBackend extends JavaPlugin implements Listener, PluginMes
 
         // Build data stream
         ByteArrayDataOutput data = ByteStreams.newDataOutput();
+        data.writeUTF(event.getPlayer().getDisplayName());
         data.writeUTF(event.getMessage());
 
         // Send plugin message
